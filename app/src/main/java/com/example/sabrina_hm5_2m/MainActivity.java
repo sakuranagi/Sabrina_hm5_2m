97ebd500061d4ab491e6da7cfd6f5e57da6c94cb
sakuranagi <sabrinamuhammadimran@gmail.com>
2023-09-21 00:48:17 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sabrina_hm5_2m/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.sabrina_hm5_2m;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Loading...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Wait!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
